de26ab316bef03ae171f91054855e2fd0a669924
Roch Schanen <r.schanen@lancaster.ac.uk>
2024-08-04 17:14:00 +0100

    prepare plpr development bench

--- plpr/plpr.vhdl
+++ plpr/plpr.vhdl
@@ -31,9 +31,9 @@ entity plpr is
     generic (size : integer := 4);                      -- factors size (4 x 4)
     port (r : in  std_logic;                            -- reset (active low)
           t : in  std_logic;                            -- trigger (rising edge)
-          a : in  std_logic_vector(size-1 downto 0);    -- input A
-          b : in  std_logic_vector(size-1 downto 0);    -- input B
-          p : out std_logic_vector(2*size-1 downto 0)); -- product AB
+          a : in  std_logic_vector(size-1 downto 0);    -- input A (4)
+          b : in  std_logic_vector(size-1 downto 0);    -- input B (4)
+          p : out std_logic_vector(2*size-1 downto 0)); -- product AB (8)
 end entity plpr;
 
 -------------------------------------------------
@@ -42,33 +42,16 @@ architecture plpr_arch of plpr is
 
 begin
 
-    -- fix null ports:
-    -- cc(0) <= '0';
+    process
+    begin
 
-    -- network elements description:
-    -- for each data bit: 
-    --   . one input pipeline
-    --   . one adder
-    --   . one output pipeline
+        p <= "00000000";
 
-    --NETWORK : for n in 0 to size-1 generate
+        -- nothing 
+        report "done";
+        wait;
 
-    --    ipl : entity fifobuf
-    --        generic map (n)
-    --        port map (r, t, i(n), ii(n));
-
-    --    add : entity addsync
-    --        port map (r, t, ii(n), oo(n), cc(n), oo(n), cc(n+1));
-
-    --    opl : entity fifobuf
-    --        generic map (size-n)
-    --        port map (r, t, oo(n), o(n));
-
-    --end generate NETWORK;
-
-    -- generate output ports
-    -- set overload flag (carry out)
-    --c <= cc(size);
+    end process;
 
 end architecture plpr_arch;
 
---
+++ plpr_bench.vhdl
@@ -0,0 +1,58 @@
+---------------------------------------------------------------------
+
+-- file: plpr_bench.vhd
+-- content: bench to simulate plpr code
+-- Created: 2024 August 4
+-- Author: Roch Schanen
+-- comments:
+
+---------------------------------------------------------------------
+
+-------------------------------------------------
+--          PIPELINED PRODUCT BENCH
+-------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+library work;
+use work.all;
+
+-------------------------------------------------
+
+entity plpr_bench is
+end plpr_bench;
+
+-------------------------------------------------
+
+architecture plpr_bench_arch of plpr_bench is
+
+    -- configuration
+
+    constant FACTORSIZE : integer := 4;
+
+    -- signals
+    signal r : std_logic; -- reset
+    signal c : std_logic; -- clock
+    signal q : std_logic_vector(2*FACTORSIZE-1 downto 0); -- output
+
+begin
+
+    -- instanciate bench clock
+    bench_clock_1: entity benchclock
+        generic map(2.5 ns) -- 200MHz
+        port map(c);
+
+    -- instanciate bench reset
+    bench_reset_1: entity benchreset
+        generic map(3 ns)
+        port map(r);
+
+    -- instanciate pipelined nco
+    pl_pr_1: entity plpr
+        generic map(FACTORSIZE)
+        port map(r, c, "0000", "0000", q);
+
+end plpr_bench_arch;
+
+-------------------------------------------------
