811f60490b96f3cdc89b84a589c33528ed6b5299
Roch Schanen <r.schanen@lancaster.ac.uk>
2024-08-14 21:34:05 +0100

    add fifo stream

--- plpr/plpr.vhdl
+++ plpr/plpr.vhdl
@@ -48,7 +48,7 @@ architecture plpr_arch of plpr is
     ---------------------------------------------------------------
     type type_1 is array (0 to N1,  0 to N1)  of std_logic;
     type type_2 is array (0 to NN1, 0 to NN1) of std_logic;
-    type type_3 is array (0 to N1,  0 to N1)  of std_logic_vector(2*N1 downto 0);
+    type type_3 is array (0 to N1,  0 to N1)  of std_logic_vector(2*NN1 downto 0);
 
     ------------------------------------------------------
     -- connections to the bit products array (SIZE x SIZE) 
@@ -63,9 +63,10 @@ architecture plpr_arch of plpr is
     signal so, co: type_2;
 
     -------------------------------------------------------------------
-    -- vector signals to the fifo stream array (SIZE x SIZE x SIZE x 4)
+    -- signal vectors to the fifo stream arrays (SIZE x SIZE x SIZE x 4)
     -- (only few connections are used, but a clean indexing is necessary)
-    signal pp: type_3;
+    -- fsa is for fifo stream array
+    signal fsa: type_3;
 
     -----------------------------------------------------------------
     -- returns the n index of bit product from the cell position i, j
@@ -87,10 +88,6 @@ architecture plpr_arch of plpr is
 
 begin
 
-
-    -- TEMP
-    p <= (others => '0');
-
     ---------------
     -- BIT PRODUCTS
     ---------------
@@ -100,167 +97,174 @@ begin
         end generate subnet_b;
     end generate subnet_a;
 
-    ----------------
-    -- INPUT STREAMS
-    ----------------
+    ------------------
+    -- INPUT STREAMS A
+    ------------------
+    subnet_fifo_A: for j in 2*SIZE-1 downto SIZE generate
 
-    --subnet_fifo_A: for j in 2*SIZE-1 downto SIZE generate
+        -- the value of i is NN1 = 2*SIZE-1
 
-    --    -- the value of i is 2*SIZE-1 <=> NN1
-
-    --    fifo_stream_mn: entity fifostream
+        fifo_stream_mn: entity fifostream
             
-    --        generic map(
-    --            -- fifo length (i+j)
-    --            NN1+j)
+            -- fifo length (i+j)
+            generic map(NN1+j)
             
-    --        port map(
-    --            -- sync signals
-    --            r, t,
-    --            -- bit product a(n) & b(m)
-    --            bp( n(NN1, j),              -- n(i, j)
-    --                m(NN1, j)),             -- m(i, j)
-    --            -- reference signal (n, m)
-    --            pp( n(NN1, j),              -- n(i, j)
-    --                m(NN1, j))              -- m(i, j)
-    --                (NN1+j downto 0));      -- i+j:0:-1
+            port map(
 
-    --end generate subnet_fifo_A;
+                -- sync signals
+                r, t,
 
-    --subnet_fifo_B: for j in N1 downto 0 generate
-    --    fifo_stream_mn: entity fifostream
-    --        generic (NN1+j) -- fifo length (i+j)
-    --        port map (r, t, -- sync signals
-    --            pi( -- bit product a(n) & b(m)
-    --                n(NN1, j),
-    --                m(NN1, j)),
-    --            pp( -- reference signal n, m, k
-    --                n(NN1, j),
-    --                m(NN1, j),
-    --                NN1+j));
-    --end generate subnet_fifo_B;
+                -- bit product a(n) & b(m)
+                bp(n(NN1, j),  -- n(i, j)
+                   m(NN1, j)), -- m(i, j)
 
-    --subnet_fifo_j: for j in 0 to SIZE-1 generate
-    --    subnet_fifo_i: for i in 0 to SIZE-1 generate
-    --    fifo_stream_mn: entity fifostream
-    --        generic (NN1+j) -- fifo length (i+j)
-    --        port map (r, t, -- sync signals
-    --            pi( -- bit product a(n) & b(m)
-    --                n(NN1, j),
-    --                m(NN1, j)),
-    --            pp( -- reference signal n, m, k
-    --                n(NN1, j),
-    --                m(NN1, j),
-    --                NN1+j));
-    --    end generate subnet_fifo_i;
-    --end generate subnet_fifo_j;
+                -- reference signal (n, m)
+                fsa(n(NN1, j),         -- n(i, j)
+                    m(NN1, j))         -- m(i, j)
+                    (NN1+j downto 0)); -- i+j:0:-1
 
-    ------------------------------
-    ---- TOP-RIGHT CELL (i=0, j=0)
-    ------------------------------
+    end generate subnet_fifo_A;
 
-    ---- instantiate input fifo buffer:
-    --fifo_in_00: entity fifobuf
-    --    generic map(0+0)            -- fifo length
-    --    port map(r, t,              -- sync signals
-    --        pi(n(0,0), m(0,0)),     -- input product a(n) & b(m)
-    --        db(0,0));               -- output to cell 0,0
+    ------------------
+    -- INPUT STREAMS B
+    ------------------
+    subnet_fifo_B: for j in SIZE-1 downto 0 generate
 
-    ---- instantiate sync adder
-    --async_00: entity addsync
-    --    port map(r, t,              -- sync signals
-    --        '0',                    -- input data A has no cell above
-    --        db(0,0),                -- input data B from input buffer
-    --        '0',                    -- carry in has no cell on the right
-    --        do(0,0),                -- data  out
-    --        co(0,0));               -- carry out
+        -- the value of i is NN1 = 2*SIZE-1
 
-    ---- instantiate output fifo buffer
-    --fifo_out_00: entity fifobuf
-    --    generic map(2*(7-0))
-    --    port map(r, t,              -- sync signals
-    --        do(0,0),                -- input from last cell
-    --        p(0));                  -- output to port
+        fifo_stream_mn: entity fifostream
+            
+            -- fifo length (i+j)
+            generic map(NN1+j)
+            
+            port map(
 
-    -------------------
-    ---- TOP LINE (j=0)
-    -------------------
-    --subnet_i0: for i in 1 to 7 generate
+                -- sync signals
+                r, t,
 
-    --    -- instantiate input fifo buffer:
-    --    fifo_in_i0: entity fifobuf
-    --        generic map(i+0)        -- fifo length
-    --        port map(r, t,          -- sync signals
-    --            pi(n(i,0), m(i,0)), -- input product a(n) & b(m)
-    --            db(i,0));           -- output to cell i,0
+                -- bit product a(n) & b(m)
+                bp(n(NN1, j),  -- n(i, j)
+                   m(NN1, j)), -- m(i, j)
 
-    --    -- instantiate sync adder
-    --    async_i0: entity addsync
-    --        port map(r, t,          -- sync signals
-    --            '0',                -- input data A has no cell above
-    --            db(i,0),            -- input data B from input buffer
-    --            co(i-1,0),          -- carry in from cell on the right
-    --            do(i,0),            -- data  out
-    --            co(i,0));           -- carry out
+                -- reference signal (n, m)
+                fsa(n(NN1, j),         -- n(i, j)
+                    m(NN1, j))         -- m(i, j)
+                    (NN1+j downto 0)); -- i+j:0:-1
 
-    --end generate subnet_i0;
+    end generate subnet_fifo_B;
 
-    -------------------------
-    ---- UPPER TRIANGLE (i>j)
-    -------------------------
-    --subnet_j: for j in 1 to 7 generate
-    --subnet_i: for i in 1 to 7 generate
-    --upper_triangle: if i > j generate
+    ---------------------
+    -- INPUT STREAMS I, J
+    ---------------------
+    subnet_fifo_j: for j in 0 to SIZE-2 generate
+        subnet_fifo_i: for i in j to j+SIZE-2 generate
 
-    --    -- instantiate input fifo buffer:
-    --    fifo_in_ij: entity fifobuf
-    --        generic map(i+j)        -- fifo length
-    --        port map(r, t,          -- sync signals
-    --            pi(n(i,j), m(i,j)), -- input product a(n) & b(m)
-    --            db(i,j));           -- output to cell i,j
+        fifo_stream_mn: entity fifostream
+            
+            -- fifo length (i+j)
+            generic map(i+j)
+            
+            port map(
 
-    --    -- instantiate sync adder
-    --    async_ij: entity addsync
-    --        port map(r, t,  -- sync signals
-    --            do(i, j-1), -- input data A from cell above
-    --            db(i, j),   -- input data B from input buffer
-    --            co(i-1, j), -- carry in from cell on the right
-    --            do(i, j),   -- data  out
-    --            co(i, j));  -- carry out
+                -- sync signals
+                r, t,
 
-    --end generate upper_triangle;
-    --end generate subnet_i;
-    --end generate subnet_j;
+                -- bit product a(n) & b(m)
+                bp(n(i, j),  -- n(i, j)
+                   m(i, j)), -- m(i, j)
 
-    ----------------------
-    ---- DIAGONALE (i = j)
-    ----------------------
-    --subnet_k: for k in 1 to 7 generate
+                -- reference signal (n, m)
+                fsa(n(i, j),         -- n(i, j)
+                    m(i, j))         -- m(i, j)
+                    (i+j downto 0)); -- i+j:0:-1
 
-    --    -- instantiate input fifo buffer:
-    --    fifo_in_kk: entity fifobuf
-    --        generic map(k+k)        -- fifo length
-    --        port map(r, t,          -- sync signals
-    --            pi(n(k,k), m(k,k)), -- input product a(n) & b(m)
-    --            db(k,k));           -- output to cell k,k
+        end generate subnet_fifo_i;
+    end generate subnet_fifo_j;
 
-    --    -- instantiate sync adder
-    --    async_kk: entity addsync
-    --        port map(r, t,  -- sync signals
-    --            do(k, k-1), -- input data A from cell above
-    --            db(k, k),   -- input data B from input buffer
-    --            '0',        -- carry has no cell on the right
-    --            do(k, k),   -- data  out
-    --            co(k, k));  -- carry out
+    ----------------------------
+    -- TOP-RIGHT CELL (i=0, j=0)
+    ----------------------------
 
-    --    -- instantiate output fifo buffer
-    --    fifo_out_kk: entity fifobuf
-    --        generic map(2*(7-k))
-    --        port map(r, t,  -- sync signals
-    --            do(k,k),    -- input from last cell
-    --            p(k));      -- output to port
+    -- instantiate sync adder
+    async_00: entity addsync
+        port map(r, t,   -- sync signals
+            '0',         -- input A has no cell above
+            fsa(0,0)(0), -- input B from fifo stream (0, 0) level 0
+            '0',         -- carry in has no cell on the right
+            so(0,0),     -- sum  out
+            co(0,0));    -- carry out
 
-    --end generate subnet_k;
+    -- instantiate output fifo buffer
+    fifo_out_00: entity fifobuf
+        generic map(2*(7-0))
+        port map(r, t, -- sync signals
+            so(0,0),   -- input from last cell
+            p(0));     -- output to port
+
+    -----------------
+    -- TOP LINE (j=0)
+    -----------------
+    subnet_i0: for i in 1 to 7 generate
+
+        -- instantiate sync adder
+        async_i0: entity addsync
+            port map(r, t,   -- sync signals
+                '0',         -- input A has no cell above
+                fsa(n(i, 0), -- input B from fifo stream (n, m) level (i+j)
+                    m(i, 0))
+                    (i+0),
+                co(i-1, 0),  -- carry in from cell on the right
+                so(i, 0),    -- sum out
+                co(i, 0));   -- carry out
+
+    end generate subnet_i0;
+
+    -----------------------
+    -- UPPER TRIANGLE (i>j)
+    -----------------------
+    subnet_j: for j in 1 to 7 generate
+        subnet_i: for i in 1 to 7 generate
+            upper_triangle: if i > j generate
+
+                -- instantiate sync adder
+                async_ij: entity addsync
+                    port map(r, t,   -- sync signals
+                        so(i, j-1),  -- input A from cell above
+                        fsa(n(i, j), -- input B from fifo stream (n, m) level (i+j)
+                            m(i, j))
+                            (i+j),
+                        co(i-1, j),  -- carry in from cell on the right
+                        so(i, j),    -- sum out
+                        co(i, j));   -- carry out
+
+            end generate upper_triangle;
+        end generate subnet_i;
+    end generate subnet_j;
+
+    --------------------
+    -- DIAGONALE (i = j)
+    --------------------
+    subnet_k: for k in 1 to 7 generate
+
+        -- instantiate sync adder
+        async_kk: entity addsync
+            port map(r, t,   -- sync signals
+                so(k, k-1),  -- input A from cell above
+                fsa(n(k, k), -- input B from fifo stream (n, m) level (i+j)
+                    m(k, k))
+                    (k+k),
+                '0',         -- carry has no cell on the right
+                so(k, k),    -- sum out
+                co(k, k));   -- carry out
+
+        -- instantiate output fifo buffer
+        fifo_out_kk: entity fifobuf
+            generic map(2*(7-k))
+            port map(r, t,  -- sync signals
+                so(k,k),    -- input from last cell
+                p(k));      -- output to port
+
+    end generate subnet_k;
 
 end architecture plpr_arch;
 
